e048b01b1c2de94871b28c76801db7522ac8fcb1
mazamora1975 <mzamora114@hotmail.com>
2023-12-27 14:30:57 -0600

    Modulo 56. Agregando estilos CSS con Bootstrap

--- src/components/InvoiceApp.jsx
+++ src/components/InvoiceApp.jsx
@@ -7,50 +7,65 @@ export const InvoiceApp = () => {
 
   return (
     <>
-      <h1>Modulo De Facturas</h1>
-      <ul>
-        <li> Id_Factura: {id}</li>
-        <li> nombre_cliente: {name}</li>
-      </ul>
+      <div className="container">
+        <div className="card my-3">
+          <div className="card-header">Modulo De Facturación</div>
+          <div className="card-body">
+            <ul className="list-group">
+              <li className="list-group-item">{id}</li>
+              <li className="list-group-item">{name}</li>
+            </ul>
 
-      <h3>Datos Del cliente</h3>
-      <ul>
-        <li>
-          {nameClient} {lastName}
-        </li>
-        <li>
-          {country}
-          {city}
-        </li>
-        <li>{street}</li>
-        <li>{number}</li>
-      </ul>
+            <div className="row my-3">
+              <div className="col">
+                <h3>Datos Del Cliente</h3>
+                <ul className="list-group">
+                  <li className="list-group-item active">{nameClient}</li>
+                  <li className="list-group-item">{lastName}</li>
+                </ul>
+                <h3>Dirección Del Cliente</h3>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Pais: {country} Ciudad: {city}
+                  </li>
+                  <li className="list-group-item">
+                    Calle: {street} Numero Exterior: {number}{" "}
+                  </li>
+                </ul>
+              </div>
 
-      <h3>Datos Fiscales De La Compañia</h3>
-      <ul>
-        <li>{company.name}</li>
-        <li>{company.fiscalNumber}</li>
-      </ul>
-      <h4>Productos De La Factura</h4>
-      <table>
-        <thead>
-          <tr>
-            <th>Products</th>
-            <th>Price</th>
-            <th>Quantity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map(({ id, product, price, quantity }) => (
-            // eslint-disable-next-line react/jsx-key
-            <tr key={id}>
-              <td>{product}</td>
-              <td>{price}</td>
-              <td>{quantity}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              <div className="col">
+                <h3>Datos De La Compañia</h3>
+                <ul className="list-group">
+                  <li className="list-group-item active">{company.name}</li>
+                  <li className="list-group-item">{company.fiscalNumber}</li>
+                </ul>
+              </div>
+            </div>
+
+            <h3>Productos De la Factura</h3>
+            <table className="table table-stripped table-hover">
+              <thead>
+                <tr>
+                  <th>Producto</th>
+                  <th>Precio</th>
+                  <th>Cantidad</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {items.map(({ id, product, price, quantity }) => (
+                  <tr key={id}>
+                    <td>{product}</td>
+                    <td>{price}</td>
+                    <td>{quantity}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
