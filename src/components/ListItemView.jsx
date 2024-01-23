c8a0278d1b845d0c35f2f082938629760e84c933
mazamora1975 <mzamora114@hotmail.com>
2024-01-23 14:39:58 -0600

    Modulo 74. Eliminar Items

--- src/components/ListItemView.jsx
+++ src/components/ListItemView.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { ProductItemView } from "./ProductItemV.iew";
 
 // eslint-disable-next-line no-unused-vars
-export const ListItemView = ({ title, items }) => {
+export const ListItemView = ({ title, items, handlerDeleteItem }) => {
   return (
     <>
       <h3>{title}</h3>
@@ -14,15 +14,18 @@ export const ListItemView = ({ title, items }) => {
             <th>Producto</th>
             <th>Precio</th>
             <th>Cantidad</th>
+            <th>Eliminar</th>
           </tr>
         </thead>
         <tbody>
           {items.map(({ id, product, price, quantity }) => (
             <ProductItemView
               key={id}
+              id={id}
               product={product}
               price={price}
               quantity={quantity}
+              handlerDeleteItem={(id) => handlerDeleteItem(id)}
             />
           ))}
         </tbody>
@@ -34,4 +37,4 @@ export const ListItemView = ({ title, items }) => {
 ListItemView.propTypes = {
   title: PropTypes.string.isRequired,
   items: PropTypes.array.isRequired,
-}
+};
